772b0786063ca4312d83cb6725f1b80659706c68
heyan <heyan@xubei.com>
2018-01-20 14:20:52 +0800

    builder pattern

---
+++ src/main/java/com/demon/builder/example1/Html.java
@@ -0,0 +1,20 @@
+package com.demon.builder.example1;
+
+import java.util.Arrays;
+
+/**
+ * Created by Administrator on 2018/1/16 0016.
+ */
+public class Html {
+
+	private String[] divide(double num){
+		long zheng = (long)num;
+		long xiao = Math.round((num-zheng)*100);
+		return new String[] {zheng+"",String.valueOf(xiao) };
+	}
+
+	public static void main(String[] args){
+		Html nr = new Html();
+		System.out.println(Arrays.toString(nr.divide(236711125.123)));
+	}
+}
---
+++ src/main/java/com/demon/builder/example1/HtmlBuilder.java
@@ -0,0 +1,15 @@
+package com.demon.builder.example1;
+
+/**
+ * 为创建文档的各个部件指定抽象接口
+ *
+ * Created by Administrator on 2018/1/16 0016.
+ */
+public interface HtmlBuilder {
+
+	void buildHead();
+
+	void buildBody();
+
+	Html buildHtml();
+}
